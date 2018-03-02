11fabd9c27f94aeb895d38a0cb81a6077b630e42
Mohamed Omar <prosentra@gmail.com>
2018-03-02 10:44:08 +0300

    Build check event function

--- game_functions.py
+++ game_functions.py
@@ -5,3 +5,8 @@ def update_screen(ai_stngs,screen, ai_ship):
 
 	#Draw spaceship
 	ai_ship.blitme()
+
+def check_events():
+	for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				turnning = False
