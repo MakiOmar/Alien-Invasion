19cc2cff00e078316769585bf07c927ad970b0eb
Mohamed Omar <prosentra@gmail.com>
2018-03-02 14:16:30 +0300

    Refactor check_events function

--- game_functions.py
+++ game_functions.py
@@ -7,23 +7,30 @@ def update_screen(ai_stngs,screen, ai_ship):
 	#Draw spaceship
 	ai_ship.blitme()
 
+def check_keydown_event(settings, event):
+	if event.type == pygame.KEYDOWN:
+		if event.key == pygame.K_RIGHT:
+			settings.move_right = True
+
+		elif event.key == pygame.K_LEFT:
+			settings.move_left = True
+def check_keyup_event(settings, event):
+	if event.type == pygame.KEYUP:
+		if event.key == pygame.K_RIGHT:
+			settings.move_right = False
+		
+		if event.key == pygame.K_LEFT:
+			settings.move_left = False
+
 def check_events(settings):
 	for event in pygame.event.get():
-			if event.type == pygame.QUIT:
-				sys.exit()
-				return
-			if event.type == pygame.KEYDOWN:
-				if event.key == pygame.K_RIGHT:
-					settings.move_right = True
+		if event.type == pygame.QUIT:
+			sys.exit()
+			return
 
-				elif event.key == pygame.K_LEFT:
-					settings.move_left = True
+		check_keydown_event(settings, event)
 
-			if event.type == pygame.KEYUP:
-				if event.key == pygame.K_RIGHT:
-					settings.move_right = False
-				
-				if event.key == pygame.K_LEFT:
-					settings.move_left = False
+		check_keyup_event(settings, event)
+			
 
 
