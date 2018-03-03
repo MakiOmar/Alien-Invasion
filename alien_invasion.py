84cf66356a3464f4721960d4dfa2b229c3837843
Mohamed Omar <prosentra@gmail.com>
2018-03-03 09:51:28 +0300

    Create Alien's instance

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import settings
 import ship
 import game_functions as GF
 from pygame.sprite import Group
+from alien import Alien
 def run_game():
 	#@ai_stngs : Alien Invaders Settings
 	ai_stngs = settings.Settings()
@@ -15,6 +16,9 @@ def run_game():
 
 	#build screen
 	screen = ai_stngs.ai_screen_size()
+
+	# Make an alien.
+ 	ai_alien = Alien(ai_settings, screen)
 
 	#game caption
 	ai_stngs.ai_caption("Alien Invasion")
@@ -35,7 +39,7 @@ def run_game():
 		 
 		GF.update_bullets(bullets)
 
-		GF.update_screen(ai_stngs,screen, ai_ship, bullets)
+		GF.update_screen(ai_stngs,screen, ai_ship, bullets, ai_alien)
 		# Make the most recently drawn screen visible.
 		pygame.display.flip()
 
