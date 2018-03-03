734a86e6c8ada34da3952105ad43a11e8eeb346f
Mohamed Omar <prosentra@gmail.com>
2018-03-03 12:38:27 +0300

    Add update function

--- alien.py
+++ alien.py
@@ -15,10 +15,15 @@ class Alien(Sprite):
 		self.rect.y = self.rect.height
 		# Store the alien's exact position.
 		self.x = float(self.rect.x)
-		
+
 		# Alien settings
  		self.alien_speed_factor = 1
 
 	def blitme(self):
 		"""Draw the alien at its current location."""
 		self.screen.blit(self.image, self.rect)
+
+	def update(self):
+		"""Move the alien right."""
+		self.x += self.ai_settings.alien_speed_factor
+		self.rect.x = self.x
