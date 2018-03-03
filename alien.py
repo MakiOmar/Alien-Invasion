8c30afb964b27d4863a67ae6d4b0f9e397f216ba
Mohamed Omar <prosentra@gmail.com>
2018-03-03 12:37:19 +0300

    Add alien speed factor

--- alien.py
+++ alien.py
@@ -15,6 +15,9 @@ class Alien(Sprite):
 		self.rect.y = self.rect.height
 		# Store the alien's exact position.
 		self.x = float(self.rect.x)
+		
+		# Alien settings
+ 		self.alien_speed_factor = 1
 
 	def blitme(self):
 		"""Draw the alien at its current location."""
