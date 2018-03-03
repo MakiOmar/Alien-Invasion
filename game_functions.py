dbc5ee5fc70c7716f834226311188e6a6512b6da
Mohamed Omar <prosentra@gmail.com>
2018-03-03 22:44:01 +0300

    Refactor update_bullets function

--- game_functions.py
+++ game_functions.py
@@ -62,6 +62,9 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
 	for bullet in bullets.copy():
 		if bullet.rect.bottom <= 0:
 			bullets.remove(bullet)
+	check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets)
+	
+def check_bullet_alien_collisions(ai_settings, screen, ship, aliens, bullets):
 	# Check for any bullets that have hit aliens.
 	# If so, get rid of the bullet and the alien.
 	collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
@@ -69,8 +72,6 @@ def update_bullets(ai_settings, screen, ship, aliens, bullets):
 		# Destroy existing bullets and create new fleet.
 		bullets.empty()
 		create_fleet(ai_settings, screen, ship, aliens)
-
-
 
 def get_number_rows(ai_settings, ship_height, alien_height):
 	"""Determine the number of rows of aliens that fit on the screen."""
