d2dde25d5885d7de9020e4fd064be3bdaa97972b
Mohamed Omar <prosentra@gmail.com>
2018-03-04 10:36:14 +0300

    Build center_ship function

--- ship.py
+++ ship.py
@@ -29,3 +29,7 @@ class Ship(object):
 			self.center -= self.ai_settings.ship_speed_factor
 
 		self.rect.centerx = self.center
+		
+	def center_ship(self):
+		"""Center the ship on the screen."""
+		self.center = self.screen_rect.centerx
