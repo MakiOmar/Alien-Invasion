2b6284cce055472ae09f5bc2b0c9f8b25a325917
Mohamed Omar <prosentra@gmail.com>
2018-03-08 09:44:11 +0300

    Initialize score value

--- game_stats.py
+++ game_stats.py
@@ -3,6 +3,10 @@ class GameStats():
 	def __init__(self, ai_settings):
 		"""Initialize statistics."""
 		self.ai_settings = ai_settings
+
+		#Initialize score value
+		self.score = 0
+
 		# Start Alien Invasion in an active state.
 		self.game_active = False
 		self.reset_stats()
