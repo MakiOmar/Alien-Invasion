2eaae3a5b6b51057062f894a23c7a2d31575ed8f
Mohamed Omar <prosentra@gmail.com>
2018-03-10 10:26:46 +0300

    Add score headings

--- scoreboard.py
+++ scoreboard.py
@@ -43,6 +43,12 @@ class Scoreboard():
 		self.score_rect_bg = pygame.Rect(0,0,120,50)
 		self.score_rect_bg.center = self.score_rect.center
 
+		self.score_heading = self.font.render("Score", True, (20, 80, 162))
+		self.score_heading_rect = self.score_heading.get_rect()
+		self.score_heading_rect.center = self.score_rect_bg.center
+		self.score_heading_rect.right = self.score_rect_bg.left-30
+
+
 	def prep_high_score(self):
 		"""Turn the high score into a rendered image."""
 		high_score = int(round(self.stats.high_score, -1))
@@ -51,12 +57,18 @@ class Scoreboard():
 
 		# Center the high score at the top of the screen.
 		self.high_score_rect = self.high_score_image.get_rect()
-		self.high_score_rect.centerx = self.screen_rect.centerx
+		self.high_score_rect.centerx = self.screen_rect.centerx+100
 		self.high_score_rect.top = self.score_rect.top
 
 		#Store rectanle area
 		self.hi_score_rect_bg = pygame.Rect(0,0,120,50)
 		self.hi_score_rect_bg.center = self.high_score_rect.center
+
+
+		self.hi_score_heading = self.font.render("High Score", True, (20, 80, 162))
+		self.hi_score_heading_rect = self.hi_score_heading.get_rect()
+		self.hi_score_heading_rect.center = self.hi_score_rect_bg.center
+		self.hi_score_heading_rect.right = self.hi_score_rect_bg.left-30
 
 	def prep_level(self):
 		"""Turn the level into a rendered image."""
@@ -64,8 +76,13 @@ class Scoreboard():
 
 		# Position the level below the score.
 		self.level_rect = self.level_image.get_rect()
-		self.level_rect.right = self.score_rect.right
-		self.level_rect.top = self.score_rect.bottom + 30
+		self.level_rect.center = self.hi_score_heading_rect.center
+		self.level_rect.right = self.hi_score_heading_rect.left - 30
+
+		self.level_heading = self.font.render("Level", True, (20, 80, 162))
+		self.level_heading_rect = self.level_heading.get_rect()
+		self.level_heading_rect.center = self.level_rect.center
+		self.level_heading_rect.right = self.level_rect.left-30
 
 	def show_score(self):
 		"""Draw scores and ships to the screen."""
@@ -79,6 +96,9 @@ class Scoreboard():
 		self.screen.blit(self.score_image, self.score_rect)
 		self.screen.blit(self.high_score_image, self.high_score_rect)
 		self.screen.blit(self.level_image, self.level_rect)
+		self.screen.blit(self.score_heading, self.score_heading_rect)
+		self.screen.blit(self.hi_score_heading, self.hi_score_heading_rect)
+		self.screen.blit(self.level_heading, self.level_heading_rect)
 
 		# Draw ships.
  		self.ships.draw(self.screen)
